3a870eaa26c98f7810c94d0a34a0808c583fe00d
john-paul-keenan <johnpaul.keenan.sf@gmail.com>
2017-12-20 17:35:16 -0800

    Fixed un/pw issue

--- create.rb
+++ create.rb
@@ -1,6 +1,12 @@
 require 'net/http'
-require 'uri'
 require 'json'
+require 'io/console'
+
+
+puts "What is your Zen Desk Username (most likley your @konghq.com email address)"
+zdUserName = gets.strip
+puts "What is your Zen Desk password?"
+zdPassword= STDIN.noecho(&:gets).chomp
 
 puts "What is the new User's Email Address?"
 newUserEmail = gets.strip
@@ -9,7 +15,7 @@ firstAndLastName = gets.strip
 
 uri = URI.parse("https://mashape.zendesk.com/api/v2/users.json")
 request = Net::HTTP::Post.new(uri)
-request.basic_auth("<yourZDUserName>", "<yourZDPassword>")
+request.basic_auth(zdUserName, zdPassword)
 request.content_type = "application/json"
 request.body = JSON.dump({
   "user" => {
@@ -27,5 +33,5 @@ response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
   http.request(request)
 end
 
-# response.code
-# response.body
+ response.code
+ response.body
